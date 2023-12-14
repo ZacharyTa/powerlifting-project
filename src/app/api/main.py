c070de9ea709a4b39f7f1a873860682aacfa8719
ZacharyTa <Zachary.C.Ta@gmail.com>
2023-12-14 12:01:54 -0800

    added pytest to requirements.txt

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -1,9 +1,11 @@
+import sys
 from database.sqlite import SQLiteDatabase
 
 def main():
     print("Main")
-    db = SQLiteDatabase()
-    db.update_db()
+    #db = SQLiteDatabase()
+    #db.update_db()
+    print(sys.executable)
     print("exitMain")
 
 if __name__ == "__main__":
