c6b6804c971bac71676ef0bea74750c7d5deaa0b
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-01-07 12:25:46 -0800

    Handle empty competition table error and added error.py under utils

---
+++ src/app/api/utils/errors.py
@@ -0,0 +1,21 @@
+
+class EmptyCompTableError(Exception):
+    
+    # Constructor
+    def __init__(self, competition_id: int, message = 'Empty Competition Table') -> None:
+        """
+        Takes competition ID from the empty competition table as an argument
+        Error Message to print is Optional
+        """
+
+        self.competition_id = competition_id
+        self.message = message
+
+    # __str__ is to print() the error message 
+    def __str__(self) -> str: 
+        return(repr(self.message)) 
+    
+    # __int__ is to p the error message 
+    # def __int__(self) -> int: 
+    #     return(int(self.competition_id)) 
+    
