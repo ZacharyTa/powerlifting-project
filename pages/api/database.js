109c5c50f48ade69211914350a67cbf87aa650e1
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-03-02 12:56:09 -0800

    Added pages/api/database.js but having a Error: self signed certificate in certificate chain

---
+++ pages/api/database.js
@@ -0,0 +1,44 @@
+import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+require('dotenv').config();
+import 'dotenv/config';
+
+export default async function handler(req, res) {
+
+    // Construct the paths to certificate files based on your environment variables
+    const caPath = path.resolve(process.env.SSL_CA_PATH);
+    const certPath = path.resolve(process.env.SSL_CERT_PATH);
+    const keyPath = path.resolve(process.env.SSL_KEY_PATH);
+
+    // Read the certificate and key files
+    const ca = fs.readFileSync(caPath, 'utf8');
+    const cert = fs.readFileSync(certPath, 'utf8');
+    const key = fs.readFileSync(keyPath, 'utf8');
+
+    // Check if the certificate files exist
+    if (!fs.existsSync(caPath) || !fs.existsSync(certPath) || !fs.existsSync(keyPath)) {
+        return res.status(500).json({ error: 'Certificate file paths are invalid or files do not exist.' });
+    }
+
+    // Connect to the MySQL database
+    const connection = await mysql.createConnection({
+        host: process.env.DB,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        ssl: {
+            ca: ca,
+            key: key,
+            cert: cert,
+        },
+    });
+
+    // Your database query goes here
+
+    const [rows] = await connection.execute('SELECT 1');
+    // Close the database connection
+    await connection.end();
+    // Send a response back to the client
+    res.status(200).json({ message: 'Connection to database was successful' });
+}
