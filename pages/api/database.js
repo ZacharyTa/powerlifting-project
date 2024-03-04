262ba6b58b7a24ce9ef1910bc56ef97b19dd47ba
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-03-03 21:26:11 -0800

    Database connection NOW working, changed create connection from await mysql.createconnection to mysql.createpool

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,6 +1,4 @@
-// import mysql from 'mysql2/promise';
-// import mysql from 'mysql2';
-const mysql = require("mysql2/promise");
+import mysql from 'mysql2/promise';
 import fs from 'fs';
 import path from 'path';
 require('dotenv').config();
@@ -41,11 +39,39 @@ export default async function handler(req, res) {
 
     // Your database query goes here
 
-    const [rows] = await connection.execute('SELECT 1');
+    // test a basic query
+    const [rows] = await connection.execute('SELECT * FROM lifts_table WHERE age = 20 LIMIT 10;'); // TypeError: (intermediate value) is not iterable. How to fix?  https://stackoverflow.com/questions/66030115/typeerror-intermediate-value-is-not-iterable-how-to-fix
     // Close the database connection
     await connection.end();
-
-    res.status(200).json(rows);
     // Send a response back to the client
+    res.status(200).json(rows);
     //res.status(200).json({ message: 'Connection to database was successful' });
 }
+
+
+// The replay may be late. But for future reference, the issue is that the transaction only works in a single connection but not in a pool. So getConnection() should be used first to get a connection from the pool. Example below:
+
+// const mysql = require('mysql2/promise'); 
+// const pool = mysql.createPool({...});
+
+// let conn = null;
+// try {
+//   conn = await pool.getConnection();
+//   await conn.query("START TRANSACTION");
+//   const [response, meta] = await conn.query("SELECT * FROM xxx FOR UPDATE");
+//   await conn.execute("INSERT INTO xxxxxx");
+//   await conn.query("COMMIT");
+// } catch (error) {
+//   if (conn) await conn.query("ROLLBACK");
+//   throw error;
+// } finally {
+//   if (conn) conn.release();
+// }
+
+// const connection = await pool.getConnection();
+// const username = req.query.username; // Assuming username comes from query parameter
+
+// const query = `SELECT * FROM users WHERE username = ?`;
+// const [rows] = await connection.execute(query, [username]);
+
+// await connection.release();
