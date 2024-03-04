d5b6ed7726762e9a20df5d6c5b4f8acc4345722b
seanquiambao <slquiambao03@gmail.com>
2024-03-04 15:11:58 -0800

    please

--- src/pages/api/compare-lifts.js
+++ src/pages/api/compare-lifts.js
@@ -7,6 +7,5 @@ export default async function handler(req, res) {
       values: [20],
     });
     res.status(200).json({ compare_lifts: results });
-    //res.status(200).json({ "Test": "Bob" });
   }
 }
