c73f9ea1a3407c6fe8cbaf07158677a41794189f
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-03-04 14:30:15 -0800

    GET Api now returns percentage of lifters the user is stronger than based on their sex, age, weight, bench, squat, deadlift. However it only returns the percentage based on total. I will add the percentage based on bench, squat, deadlift separately later (Maybe return it as a list?). It also only works with hardcoded variables.

--- src/pages/api/compare-lifts.js
+++ src/pages/api/compare-lifts.js
@@ -1,25 +1,41 @@
 import { query } from "@/lib/database";
 import getAgeDivID from "@/utils/getAgeDivID";
 import getWeightDivID from "@/utils/getWeightDivID";
-const csv = require('csv-parser')
-const fs = require('fs')
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
-        // Hardcoded for testing development purposes
-        let age = 19;
+        // Hardcoded for testing development purposes (Forms data will be used here)
+        let age = 23;
         let sex = 0;
-        let weight = 70;
+        let weight = 54.2;
+        let bench = 100;
+        let squat = 300;
+        let deadlift = 100;
+        let total = bench + squat + deadlift;
+
+        // Get the age_div_id and weight_div_id
         const ageDivID = await getAgeDivID(age);
         const weightDivID = await getWeightDivID(sex, weight, age);
 
-        res.status(200).json({ageDivID, weightDivID})
-
-    //     const results = await query({
-    //         query: 'SELECT * FROM lifts_table WHERE age < ? LIMIT 10;',
-    //         values: [20], //Hardcoded for testing purposes
-    // });
-        //res.status(200).json({compare_lifts: results});
-        //res.status(200).json({ "Test": "Bob" });
+        //Calculate the percentage of lifters stronger than the user within the same age and weight division
+        let compare_query = 'SELECT ' +
+        '(' +
+          '(SELECT COUNT(DISTINCT lifter_id) FROM lifts_table  ' +
+          'WHERE ? > total ' +
+          'AND age_div_id = ? ' +
+          'AND weight_div_id = ? ' +
+          ') / ' +
+          '(SELECT COUNT(DISTINCT lifter_id) FROM lifts_table ' +
+          'WHERE age_div_id = ? ' +
+          'AND weight_div_id = ? ' + 
+          ') * 100 ' +
+        ') AS percentage_of_lifters_stronger_than_x ';
+        // res.status(200).json({compare_query});
+        const results = await query({
+            query: compare_query,
+            values: [total, ageDivID, weightDivID, ageDivID, weightDivID],
+        });
+        res.status(200).json({compare_lifts: results});
+        //return results;
     }
 }
