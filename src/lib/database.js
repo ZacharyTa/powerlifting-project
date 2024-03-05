e76a6d3c44ae916162e880af01c174d9e7abde42
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-03-05 08:58:47 -0800

    Save gitignore and database.js file

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,46 +1,49 @@
-import mysql from 'mysql2/promise';
-import fs from 'fs';
-import path from 'path';
+import mysql from "mysql2/promise";
+import fs from "fs";
+import path from "path";
 
 export async function query({ query, values = [] }) {
+  // Construct the paths to certificate files based on your environment variables
+  const caPath = path.resolve(process.env.SSL_CA_PATH);
+  const certPath = path.resolve(process.env.SSL_CERT_PATH);
+  const keyPath = path.resolve(process.env.SSL_KEY_PATH);
 
-    // Construct the paths to certificate files based on your environment variables
-    const caPath = path.resolve(process.env.SSL_CA_PATH);
-    const certPath = path.resolve(process.env.SSL_CERT_PATH);
-    const keyPath = path.resolve(process.env.SSL_KEY_PATH);
+  // Read the certificate and key files
+  const ca = fs.readFileSync(caPath, "utf8");
+  const cert = fs.readFileSync(certPath, "utf8");
+  const key = fs.readFileSync(keyPath, "utf8");
 
-    // Read the certificate and key files
-    const ca = fs.readFileSync(caPath, 'utf8');
-    const cert = fs.readFileSync(certPath, 'utf8');
-    const key = fs.readFileSync(keyPath, 'utf8');
+  // Check if the certificate files exist
+  if (
+    !fs.existsSync(caPath) ||
+    !fs.existsSync(certPath) ||
+    !fs.existsSync(keyPath)
+  ) {
+    return res.status(500).json({
+      error: "Certificate file paths are invalid or files do not exist.",
+    });
+  }
+  try {
+    // Connect to the MySQL database
+    const db_connection = mysql.createPool({
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      ssl: {
+        ca: ca,
+        key: key,
+        cert: cert,
+        // Bypass verification False (development only): Remove later
+        rejectUnauthorized: true,
+      },
+    });
 
-    // Check if the certificate files exist
-    if (!fs.existsSync(caPath) || !fs.existsSync(certPath) || !fs.existsSync(keyPath)) {
-        return res.status(500).json({ error: 'Certificate file paths are invalid or files do not exist.' });
-    }
-    try {
-
-        // Connect to the MySQL database
-        const db_connection = mysql.createPool({
-            port: process.env.DB_PORT,
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME,
-            ssl: {
-                ca: ca,
-                key: key,
-                cert: cert,
-                // Bypass verification False (development only): Remove later
-                rejectUnauthorized: true,
-            },
-        });
-
-        const [results] = await db_connection.execute(query, values);
+    const [results] = await db_connection.execute(query, values);
 
     return results;
-
-    } catch (error) {
-        throw Error(error.message);
-    }
+  } catch (error) {
+    throw Error(error.message);
+  }
 }
