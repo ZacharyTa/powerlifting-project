c44704971281db89d4992464e1d94e36805153ff
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-03-09 19:29:04 -0800

    toast + cleanup

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,18 +1,21 @@
-"use client";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 import Radio from "@/components/Radio";
 import { ATTRIBUTES, SEX, UNIT } from "@/data/calculator/Calculations";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Calculator = ({ onCalculate }) => {
-  const [data, setData] = useState(ATTRIBUTES);
+  const [input, setInput] = useState(ATTRIBUTES);
 
   const handleSubmit = async () => {
+    if (!Object.values(input).every((value) => value)) {
+      toast("❌ Please fill out everything.");
+      return;
+    }
     try {
-      data.sex;
       const response = await fetch(
-        `/api/comparePercents?age=${data.age}&sex=${data.sex}&unit=${data.unit}&weight=${data.weight}&bench=${data.bench}&squat=${data.squat}&deadlift=${data.deadlift}`,
+        `/api/comparePercents?age=${input.age}&sex=${input.sex}&unit=${input.unit}&weight=${input.weight}&bench=${input.bench}&squat=${input.squat}&deadlift=${input.deadlift}`,
         {
           method: "GET",
           headers: {
@@ -21,14 +24,15 @@ const Calculator = ({ onCalculate }) => {
         },
       );
 
-      const responseData = await response.json();
+      const data = await response.json();
 
       if (!response.ok) {
-        throw new Error(`HTTP error: status: ${response.status}`);
+        toast(`❌ ${response.status}`);
+        return;
       }
 
       // Pass the response data to the onCalculate function
-      onCalculate(responseData.compareLifts[0]);
+      onCalculate(data.compareLifts[0]);
     } catch (error) {
       console.error("A problem occurred when making the API call:", error);
     }
@@ -37,43 +41,48 @@ const Calculator = ({ onCalculate }) => {
   return (
     <div className="flex flex-col gap-y-7 items-center">
       <div className="flex flex-row gap-x-5">
-        <Input name="age" maxLength={3} data={data} setData={setData} />
-        <Radio options={SEX} field="sex" current={data} setCurrent={setData} />
+        <Input name="age" maxLength={3} data={input} setData={setInput} />
+        <Radio
+          options={SEX}
+          field="sex"
+          current={input}
+          setCurrent={setInput}
+        />
       </div>
       <>
         <Radio
           options={UNIT}
           field="unit"
-          current={data}
-          setCurrent={setData}
+          current={input}
+          setCurrent={setInput}
         />
         <Input
           name="weight"
           maxLength={3}
-          data={data}
+          data={input}
           isDecimal={true}
-          setData={setData}
+          setData={setInput}
         />
         <Input
           name="squat"
           maxLength={4}
-          data={data}
+          data={input}
           isDecimal={true}
-          setData={setData}
+          setData={setInput}
         />
         <Input
           name="bench"
           maxLength={4}
-          data={data}
+          data={input}
           isDecimal={true}
-          setData={setData}
+          setData={setInput}
         />
         <Input
           name="deadlift"
           maxLength={4}
-          data={data}
+          data={input}
           isDecimal={true}
-          setData={setData}
+          setData={setInput}
         />
       </>
 
