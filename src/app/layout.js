4a66d07e983f39ddb7beebbbdeb1ba14f3c67361
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-04-08 22:27:35 -0700

    Added Umami cloud script to trach analytics. Also changed favico.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
+import Head from "next/head";
 import { Nunito } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -18,6 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          defer
+          src="https://analytics.us.umami.is/script.js"
+          data-website-id="3e5c516d-d699-4bef-9aac-cf32e92e3c86"
+        ></script>
+      </Head>
       <body
         className={`${nunito.className} bg-power-white text-power-black h-screen`}
       >
